0c371eb59d65972f35d82063175da53a96fb2581
piyush0049 <piyushjoshi81204@gmail.com>
2025-09-01 21:57:06 +0530

    code cleaning

--- src/pages/api/user/[id].js
+++ src/pages/api/user/[id].js
@@ -23,8 +23,6 @@ export default async function handler(req, res) {
       } catch (err) {
         return res.status(401).json({ message: 'Invalid token' });
       }
-
-      // ✅ Fetch public profile details only
       const otherUser = await User.findById(id).select("_id name image bio interests createdAt");
 
       if (!otherUser) {
