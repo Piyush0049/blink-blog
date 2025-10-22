c5bec75e4a593101950557a820e0f0b7a1ae537a
piyush0049 <piyushjoshi81204@gmail.com>
2025-10-22 18:46:46 +0530

    redis removal

--- src/pages/api/allblogsroute.js
+++ src/pages/api/allblogsroute.js
@@ -1,26 +1,6 @@
 import connectToDatabase from '@/config/db';
 import Blog from '@/models/blogmodel';
 import User from '@/models/usermodel';
-// import { createClient } from "redis";
-
-// const client = createClient({
-//   url: process.env.REDIS_URL,
-//   socket: {
-//     tls: process.env.REDIS_URL.startsWith("rediss://"),
-//     reconnectStrategy: (retries) => Math.min(retries * 50, 500),
-//   },
-// });
-
-// let redisConnected = false;
-
-// async function connectRedis() {
-//   if (!redisConnected) {
-//     client.on("connect", () => console.log("✅ Redis connected"));
-//     client.on("error", (err) => console.error("❌ Redis Error", err));
-//     await client.connect();
-//     redisConnected = true;
-//   }
-// }
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
